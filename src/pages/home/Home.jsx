ece8755dee80468b6dbefceeaa83e7bf2eabcbe5
Maseyitov <masejitovandaniel@gmail.com>
2026-06-14 10:43:40 +0600

    finish

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import 'react'
 import AboutCompany from '../../widgets/aboutcompany/AboutCompany'
+// import News from '../news/News'
+// import Apartments from '../Apartments/Apartments'
 
 function Home() {
   return (
     <>
       homr
       <AboutCompany />
+      {/* <Apartments /> */}
+      {/* <News /> */}
     </>
   )
 }
